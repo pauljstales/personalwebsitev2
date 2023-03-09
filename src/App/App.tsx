bc2cbc74f33faaf34d0cbdaadf24b67cb0e67851
Paul J Stales <pauljstales@gmail.com>
2023-03-09 02:59:05 -0500

    corrected sizing via media query

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -42,56 +42,53 @@ function App() {
                 <h1>History of p.js</h1>
                 <p>
                     My original college major was mechanical engineering. While interesting and enjoyable, I took a
-                    programming course and was fascinated. I knew early on I had to switch majors.{' '}
-                </p>
-                <p>
-                    My first job was mostly Java. I saw a colleague filling in a form,{' '}
+                    programming course and was fascinated. I knew early on I had to switch majors. My first job was
+                    mostly Java. I saw a colleague filling in a form,{' '}
                     <em>and the rest of the screen updated, like magic</em>. It was AngularJS, and just as I knew I
                     needed to leave mechanical engineering for software, I had to leave Java for JavaScript.
                 </p>
                 <p>
                     Colleagues took notice of my passion for JavaScript and my growing contributions to various
-                    projects. One of them noticed my full name and mused "P... JS. You were meant to write JavaScript."
+                    projects. One of them noticed my full name and mused "
+                    <em>P... JS. You were meant to write JavaScript.</em>" And so I did.
                 </p>
-                <p>And so I did.</p>
             </header>
             <section className='section'>
-                <h1>Full-Stack Dev</h1>
+                <h1>Full Stack Dev</h1>
                 <p>I love the entire world of software, from the edge of the web to the depths of the database.</p>
-                <p>
-                    I use <b>reactjs</b> for the frontend. I know CSS3 flexbox and grid, though these days I prefer to
-                    use <b>TailwindCSS</b>.
-                </p>
-                <p>
-                    I have years of experience in backend development in Java and the Spring ecosystem, that translated
-                    well into <b>nodejs</b> and <b>expressjs</b>.
-                </p>
-                <p>
-                    I know both <b>JavaScript</b> and <b>TypeScript</b>.
-                </p>
-                <p>
-                    I have more experience with relational databases than NoSQL ones, but I can work just fine in
-                    MongoDB and others.
-                </p>
-                <p>
-                    I know enough <b>Amazon Web Services</b> to be dangerous, and wish to learn more.
-                </p>
+                <ul>
+                    <li>
+                        Frontend: I use <b>reactjs</b> for the frontend. I know CSS3 flexbox and grid, though these days
+                        I prefer to use <b>TailwindCSS</b>
+                    </li>
+                    <li>
+                        Backend: I use <b>nodejs</b> and <b>express</b> these days, but I was Java/Spring for several
+                        years (not anymore!)
+                    </li>
+                    <li>
+                        Persistence: I am more familiar with relational databases such as <b>PostgreSQL</b> than NoSQL
+                        ones, but I can get around in <b>MongoDB</b>
+                    </li>
+                    <li>
+                        Cloud: I know enough of the <b>Amazon Web Services (AWS)</b> to be dangerous, and I cannot wait
+                        to learn more
+                    </li>
+                </ul>
             </section>
             <section className='section'>
                 <h1>My Goal</h1>
-                <p>Create software that has a positive impact on people's lives.</p>
-                <p>What kind of positive impact? Software that</p>
+                <p>Create software that has a positive impact on people's lives:</p>
                 <ul>
-                    <li>save people time</li>
-                    <li>helps them achieve their goals</li>
-                    <li>makes or saves them money</li>
-                    <li>gives them a pleasant escape</li>
+                    <li>saving time on tasks</li>
+                    <li>achieving their goals</li>
+                    <li>making/saving money</li>
+                    <li>enjoying a pleasant escape</li>
                 </ul>
             </section>
             <section className='section'>
                 <h1>Working with Me</h1>
-                <p>My colleagues would describe me as passionate, thorough, and clear.</p>
                 <p>
+                    My colleagues would describe me as passionate, thorough, and clear. <br />
                     But you can read it for yourself, on my{' '}
                     <a
                         target='_blank'
@@ -123,6 +120,10 @@ function App() {
                     </li>
                 </ul>
             </section>
+            <footer className='section'>
+                <hr />
+                <small>p.js 2023, v2, new website in the works!</small>
+            </footer>
         </>
     );
 }
